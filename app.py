2fc1c4b8fd0d7918f82fcb9aa9cedcdca51000ab
Dzianis Bohush <110992506+densparrow@users.noreply.github.com>
2022-10-19 20:02:38 +0300

    Создал базу данных shop.db

--- app.py
+++ app.py
@@ -3,13 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
-db = SQLAlchemy
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     price = db.Column(db.Integer, nullable=False)
     category = db.Column(db.String(100), nullable=False)
+    text = db.Column(db.Text, nullable=False)
     isActive = db.Column(db.Boolean, default=True)
 
 @app.route('/')
